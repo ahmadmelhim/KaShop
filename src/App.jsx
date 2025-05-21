ff1f011be5239084d3175017f4c95e10edcf1827
Your Name <you@example.com>
2025-05-21 20:11:33 +0300

    Create Routing

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { RouterProvider } from 'react-router'
+import routes from './routes.jsx';
+
+export default function App() {
+  return (
+    <RouterProvider router={routes} />
+
+  )
+}
---
+++ src/routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from 'react-router';
+import MainLayout from "./layout/MainLayout";
+import ErrorPage from "./pages/error/ErrorPage";
+import Home from './pages/home/Home';
+import Shop from './pages/shop/Shop';
+import Cart from './pages/cart/Cart';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+const routes= createBrowserRouter([
+    {
+        path:'/',
+        element: <MainLayout />,
+        errorElement:<ErrorPage />,
+        children:[
+            {
+                path:'/',
+                element :<Home />
+            },
+            {
+                path:'/shop',
+                element:<Shop /> 
+            },
+            {
+                path:'/cart',
+                element:<Cart />
+            },
+            {
+                path:'/login',
+                element:<Login/>
+            },
+            {
+                path:'/register',
+                element:<Register/>
+            }
+        ]
+    }
+]);
+export default routes;
